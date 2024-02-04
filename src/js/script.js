9fd31ad65a82c18a5ed53538ecf3170a816df414
Luis Alva Celis <40306835+luisalvacelis@users.noreply.github.com>
2024-02-04 03:03:00 -0500

    diseño de escritorio terminado

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,43 @@
+AOS.init({
+  duration: 1200,
+});
+
 var owl = $(".owl-carousel");
 
 const username = "luisalvacelis";
 const apiUrl = `https://api.github.com/users/${username}/repos`;
+
+$.get(apiUrl, function (repositories) {
+  const repositoriesDiv = $("#repositories-desktop");
+  repositories.forEach((repository) => {
+    const originalDate = new Date(repository.updated_at);
+    const dateFormat = new Intl.DateTimeFormat("es-Es", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      timeZone: "UTC",
+    });
+    const formattedDate = dateFormat.format(originalDate);
+    const repoArticle = `<div class="portfolio-card" style="width: 18rem">
+    <div class="portfolio-card-body">
+      <h5 class="portfolio-card-title">${repository.name}</h5>
+      <h6 class="portfolio-card-subtitle mb-2 text-muted">${formattedDate}</h6>
+      <p class="portfolio-card-text">
+        <em>${repository.language}</em> - ${
+      repository.description || "Descripción no disponible"
+    }
+      </p>
+      <a href="${
+        repository.html_url
+      }" target="_blank" class="btn btn-primary portfolio-card-link">Ver en GitHub</a>
+    </div>
+  </div>`;
+    repositoriesDiv.append(repoArticle);
+  });
+});
 
 $.get(apiUrl, function (repositories) {
   const repositoriesDiv = $("#repositories");
@@ -25,9 +61,7 @@ $.get(apiUrl, function (repositories) {
             <p class="portfolio--card-description">
               ${repository.description || "Descripción no disponible"}
             </p>
-            <a href="${
-              repository.html_url
-            }" target="_blank" class="btn btn-primary">Ver en Github</a>
+            <a href="" target="_blank" class="btn btn-primary">Ver en Github</a>
           </article>`;
 
     repositoriesDiv.append(repoArticle);
